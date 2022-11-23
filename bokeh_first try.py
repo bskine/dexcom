f2b0ed428d89b04a5daa543ba8e2aded85356f9f
bskine <bskine@gmail.com>
2022-11-23 11:56:13 -0900

    rainbow bar graph working

--- bokeh_first try.py
+++ bokeh_first try.py
@@ -2,8 +2,13 @@ import averages
 
 from bokeh.io import output_file
 from bokeh.plotting import figure, show
+from bokeh.transform import factor_cmap
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import Spectral6
 # from bokeh.layouts import row, column, gridplot
 # import bokeh.models.widgets
+
+output_file('average_bar_graph.html', title='Average Blood Glucose')
 
 top = [averages.glucose_current()[1],
        averages.glucose_average_1hour()[1],
@@ -15,7 +20,7 @@ top = [averages.glucose_current()[1],
 tags = ['current', '1Hour', '3hour', '6Hour', '12Hour', '24Hour']
 x = [tags[i] for i in range(0, 6)]
 
-output_file('average_bar_graph.html', title='Average Blood Glucose')
+source = ColumnDataSource(data=dict(x=x, top=top))
 
 fig = figure(title='CURRENT AND AVERAGE BLOOD GLUCOSE',
              height=500, width=500,
@@ -27,7 +32,8 @@ fig = figure(title='CURRENT AND AVERAGE BLOOD GLUCOSE',
              )
 
 fig.vbar(x, top=top,
-         color='blue',
+         # color='blue',
+         fill_color=factor_cmap('x', palette=Spectral6, factors=x),
          width=0.75
          )
 

abc06f09a38aabeb17a62735829617079aa0d5ab
bskine <bskine@gmail.com>
2022-11-23 11:40:39 -0900

    cleanup up and working, good rollback point

--- bokeh_first try.py
+++ bokeh_first try.py
@@ -2,16 +2,8 @@ import averages
 
 from bokeh.io import output_file
 from bokeh.plotting import figure, show
-# from bokeh.models import ColumnDataSource
 # from bokeh.layouts import row, column, gridplot
 # import bokeh.models.widgets
-
-# x = [averages.glucose_current()[0],
-#      averages.glucose_average_1hour()[0],
-#      averages.glucose_average_3hour()[0],
-#      averages.glucose_average_6hour()[0],
-#      averages.glucose_average_12hour()[0],
-#      averages.glucose_average_24hour()[0]]
 
 top = [averages.glucose_current()[1],
        averages.glucose_average_1hour()[1],
@@ -29,7 +21,6 @@ fig = figure(title='CURRENT AND AVERAGE BLOOD GLUCOSE',
              height=500, width=500,
              x_range=x,
              y_range=(0, 300),
-             # x_axis_type='log',
              x_axis_location='below',
              x_axis_label='hourly average',
              y_axis_label='mg/dl'
